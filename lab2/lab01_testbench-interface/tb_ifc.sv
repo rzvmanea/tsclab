d031d13ddf94c5069d3ab5e38eb29f6365917371
rzv <razvan.manea@student.unitbv.ro>
2022-03-20 20:11:19 -0400

    commit lab 2 si 3

---
+++ lab2/lab01_testbench-interface/tb_ifc.sv
@@ -0,0 +1,20 @@
+/***********************************************************************
+ * A SystemVerilog testbench for an instruction register; This file
+ * contains the interface to connect the testbench to the design
+ **********************************************************************/
+interface tb_ifc (input logic clk);
+  timeunit 1ns/1ns;
+
+  // user-defined types are defined in instr_register_pkg.sv
+  import instr_register_pkg::*;
+
+  logic          load_en;
+  logic          reset_n;
+  opcode_t       opcode;
+  operand_t      operand_a, operand_b;
+  address_t      write_pointer, read_pointer;
+  instruction_t  instruction_word;
+
+
+endinterface: tb_ifc
+
